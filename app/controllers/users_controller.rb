e2a454748b2ff0b79a7facadc770af9d5df81d32
Niko Lagman <lagman.niko@gmail.com>
2024-11-05 16:04:43 -0500

    Add user trackable information
    - Enable trackable module in User model:
      devise :trackable
    
    - Add trackable fields to users table:
      - sign_in_count
      - current_sign_in_at
      - last_sign_in_at
      - current_sign_in_ip
      - last_sign_in_ip
    
    - Update users#show to include trackable information in response
    
    These changes enable tracking of user sign-in activity and expose
    this information through the API.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    render json: current_user, only: [ :id, :email ]
+    render json: current_user, only: [
+      :id, 
+      :email,
+      :sign_in_count,
+      :current_sign_in_at,
+      :last_sign_in_at,
+      :current_sign_in_ip,
+      :last_sign_in_ip
+    ]
   end
 end
